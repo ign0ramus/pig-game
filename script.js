0422f9a9bbf5eeb28c7db66c4ea94842da4ed373
ign0ramus <yanushkevich_dmitry@mail.ru>
2018-09-18 19:27:29 +0300

    did some refactoring

--- script.js
+++ script.js
@@ -6,18 +6,15 @@ $(() => {
 
   $(".ok").click(toggleHide("#welcome-div"));
   $("#rules").click(toggleHide("#welcome-div"));
-  $(".lang").click(toggleHide(".label"));
-  $(".lang").click(toggleHide($("#welcome-div").children()));
-  $(".lang").click(() => {
+  $(".lang").click(toggleHide(".label")).click(toggleHide($("#welcome-div").children())).click(() => {
     // устанавливает дефолтное имя игрока в соответствии с выбранным языком
-    const player1 = $("#player1");
-    const player2 = $("#player2");
-    if ( player1.val() === "Игрок 1" && player2.val() === "Игрок 2") {
-      player1.val("Player 1");
-      player2.val("Player 2");
-    } else {
-      player1.val("Игрок 1");
-      player2.val("Игрок 2");
+    for (let i = 1; i <= 2; i++) {
+      let player = $("#player" + i);
+      if ( player.val() === "Игрок " + i) {
+        player.val("Player " + i);
+      } else {
+        player.val("Игрок " + i);
+      }
     }
   });
 
@@ -25,31 +22,24 @@ $(() => {
     /* обрабатывается бросок кости и все связанные процессы в счете,
     становится доступным удержание счета */
     const valToInt = (selector) => Number.parseInt($(selector).text());
-    const showDice = () => {
-      $("#dice-img").attr("src", "images/dice-" + diceSide.toString() + ".png").removeClass("hide");
-    };
-    const toggleActivePlayer = () => {
-      $(".panel").toggleClass("active");
-    };
-    const offHoldBtn = () => {
-      $("#hold").off("click");
-    };
+    const toggleActivePlayer = () => $(".panel").toggleClass("active");
+    const offHoldBtn = () => $("#hold").off("click");
     let totalScoreSelector = ".active .total-score span";
     let currentScoreSelector = ".active .current-box .current-score";
     let totalScore = valToInt(totalScoreSelector);
-
     let diceSide = Math.floor(Math.random() * 6) + 1;
+
     if (totalScore < 100) {
+      $("#dice-img").attr("src", "images/dice-" + diceSide + ".png").removeClass("hide");
       if (diceSide !== 1) {
         let currentScore= valToInt(currentScoreSelector);
         currentScore += diceSide;
-        showDice();
         changeText(currentScoreSelector, currentScore);
 
         $("#hold").click((event) => {
           event.stopImmediatePropagation(event);
           totalScore += valToInt(currentScoreSelector);
-          changeText(currentScoreSelector, "0");
+          changeText(currentScoreSelector, 0);
           changeText(totalScoreSelector, totalScore);
           if (totalScore >= 100) {
             $(".active .win").removeClass("winner-text-hide");
@@ -61,7 +51,6 @@ $(() => {
         });
 
       } else {
-        showDice();
         changeText(currentScoreSelector, 0);
         offHoldBtn();
         toggleActivePlayer();
@@ -69,16 +58,12 @@ $(() => {
     }
   });
   $("#new-game").click(() => {
-    changeText(".player-1 .total-score span", 0);
-    changeText(".player-1 .current-box .current-score", 0);
-    changeText(".player-2 .total-score span", 0);
-    changeText(".player-2 .current-box .current-score", 0);
+    for (let i = 1; i <= 2; i++) {
+      changeText(".player-" + i + " .total-score span", 0);
+      changeText(".player-" + i + " .current-box .current-score", 0);
+    }
     $(".active .win").addClass("winner-text-hide");
-    if ($(".player-2").hasClass("active")) {
-      $(".panel").toggleClass("active");
-    }
-    if (!$("#dice-img").hasClass("hide")) {
-      $("#dice-img").addClass("hide");
-    }
+    $(".player-2").hasClass("active") ? $(".panel").toggleClass("active") : 0;
+    $("#dice-img").addClass("hide");
   });
 });
