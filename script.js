563caeccb9b55cc00ef7c7281805c38261809e26
ign0ramus <yanushkevich_dmitry@mail.ru>
2018-09-19 19:29:13 +0300

    added cool animation for dices

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(() => {
   const toggleHide = (selector) => () => $(selector).toggleClass("hide");
   const changeText = (selector, newtext) => $(selector).text(newtext);
   const toggleActivePlayer = () => $(".panel").toggleClass("active");
-  const hideDiceImg = () => $("#dice-img").addClass("hide");
 
   $(".ok").click(toggleHide("#welcome-div"));
   $("#rules").click(toggleHide("#welcome-div"));
@@ -25,27 +24,58 @@ $(() => {
     let currentScoreSelector = ".active .current-box .current-score";
     let totalScore = valToInt(totalScoreSelector);
     let diceSide = Math.floor(Math.random() * 6) + 1;
-
+    const dice3d = (diceSide) => {
+      let directX = 720;
+      let directY = 720;
+      switch(diceSide) {
+        case 1: 
+            break; 
+        case 2: 
+            directX += 270;
+            break;
+        case 5:
+            directX += 90;
+            break;
+        case 6: 
+            directX += 180;
+        }
+        switch(diceSide) {
+            case 3: 
+                directY += 90;
+                break;
+            case 4:
+                directY += 270;
+                break;
+      }
+      $(".cube").css({"transition" : "none","transform" : "rotateX(0deg) rotateY(0deg)"});
+      $("#roll").css({"pointer-events" : "none"});
+      setTimeout(() => {
+        $(".cube").css({"transition" : "transform 4s cubic-bezier(.01,.27,.56,.98)","transform" : "rotateX(" + directX + "deg) rotateY(" + directY + "deg)"});
+        $(".cube").removeClass("backt-to-1");
+      });
+      setTimeout(() => $("#roll").css({"pointer-events" : "auto"}), 4000);
+    }
     if (totalScore < 100) {
-      $("#dice-img").attr("src", "images/dice-" + diceSide + ".png").removeClass("hide");
-      $("#hold").click((event) => {
+      dice3d(diceSide);
+      setTimeout(() => {
+        $("#hold").click((event) => {
         event.stopImmediatePropagation(event);
         totalScore += valToInt(currentScoreSelector);
         changeText(currentScoreSelector, 0);
         changeText(totalScoreSelector, totalScore);
         totalScore >= 100 ? $(".active .win").removeClass("winner-text-hide") : toggleActivePlayer();
-        hideDiceImg();
         $("#hold").off("click");
         $("#roll").show();
         $(".unlucky").addClass("roll-close");
-      });
+        });
+      }, 4200);
       if (diceSide !== 1) {
-        let currentScore= valToInt(currentScoreSelector) + diceSide;
-        changeText(currentScoreSelector, currentScore);
+        let currentScore = valToInt(currentScoreSelector) + diceSide;
+        setTimeout(() => changeText(currentScoreSelector, currentScore), 4000);
       } else {
-        changeText(currentScoreSelector, 0);
-        $("#roll").hide();
-        $(".unlucky").removeClass("roll-close");
+        setTimeout(() => changeText(currentScoreSelector, 0), 4000);
+        setTimeout(() => $("#roll").hide(), 4000);
+        setTimeout(() => $(".unlucky").removeClass("roll-close"), 4000);
       }
     }
   });
@@ -55,7 +85,6 @@ $(() => {
       changeText(".player-" + i + " .current-box .current-score", 0);
     }
     $(".active .win").addClass("winner-text-hide");
-    hideDiceImg();
     $(".player-2").hasClass("active") ? toggleActivePlayer() : 0;
   });
 });
